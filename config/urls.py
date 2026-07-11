32bc47c41c37c7c62922133a86e68c4eb45673af
Adonis Rodrigues <adonisrodrigues1999@gmail.com>
2026-07-11 14:54:16 +0530

    Add email secret and fallback redirects

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+from django.urls import include, path, re_path
+
+
+def redirect_unknown(request, path=None):
+    if request.user.is_authenticated:
+        if request.user.role == 'admin':
+            return redirect('admin_dashboard')
+        if request.user.role == 'owner':
+            return redirect('owner_dashboard')
+        return redirect('customer_dashboard')
+    return redirect('login')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +21,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
 
     path('', include('bookings.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
+    re_path(r'^(?P<path>.*)$', redirect_unknown),
+]
